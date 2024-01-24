831ce600022d49bf6e255ff3d3a273fe8d36eebb
ausprouty <bob.prouty@powertochange.org.au>
2024-01-24 13:06:02 +1100

    images updated

--- src/pages/ViewTract.vue
+++ src/pages/ViewTract.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page padding>
+    <img src="/src/assets/tracts/images/heart_1.png">
    <p>{{this.$route.params.tract}}</p>
    <div v-html="tractContent"></div>
     <!-- content -->
@@ -35,3 +36,399 @@ export default {
   },
 }
 </script>
+<style scoped>
+
+
+/*
+** For Here's Life Tracts
+*/
+
+
+@font-face {
+    font-family: 'dinaminauniwebregular';
+    src: url('web12-webfont.woff2') format('woff2'),
+         url('web12-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+.sinhala{
+	font-family:  dinaminauniwebregular, Arial, sans-serif;
+}
+
+@font-face {
+    font-family: 'noto_sans_tibetan';
+    src: url('notosanstibetan-regular-webfont.woff2') format('woff2'),
+         url('notosanstibetan-regular-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
+.tibetian{
+	font-family:  noto_sans_tibetan, Arial, sans-serif;
+}
+
+/*
+** colors for div
+*/
+.tract-title{
+	padding-top:15px;
+	}
+.section-title{
+	background-color: #fff;
+	font-size: 1.2em;
+	line-height: 1.3em;
+	color: #0d9ecc;
+	color: #005c86;
+	white-space: normal;
+	text-align: left;
+	position: relative;
+	display:table-cell;
+}
+.section-intro,
+.section-law1,
+.section-law2,
+.section-law3,
+.section-law4,
+.section-circles,
+.div-circles,
+.section-prayer,
+.section-know,
+.section-nowthat,
+.section-grow,
+.section-feel,
+.section-church,
+.section-contact
+{
+	background-color: #fff;
+	border:none;
+}
+tr.even,
+tr.odd,
+tr:nth-child(2n+2),
+tr.info,
+tr.tract-row{
+	background-color: #fff;
+}
+.section-copyright{
+	padding-top: 40px;
+	padding-left: 15px;
+	padding-right: 15px;
+
+
+}
+.nodisplay{
+	display:none;
+}
+
+table {
+  table-layout: fixed;
+}
+#tract-table{
+	border:none;
+	background-color: #fff;
+}
+
+tbody tr {
+  border-top:none;
+}
+.tract-header{
+	background-color: #FFF;
+	border:none;
+}
+
+tr.tract-row{
+	vertical-align: top;
+}
+
+td{
+	vertical-align:top;
+	padding-bottom: 1em;
+}
+td.side-left,
+th.side-left {
+  border-bottom: 0;
+  padding-left: 5px;
+  width: 50%;
+  padding-right: 30px;
+}
+td.side-right,
+th.side-right {
+  border-bottom: 0;
+  width: 50%;
+  padding-left: 30px;
+  padding-right: 5px;
+}
+td.side-whole,
+th.side-whole {
+  border-bottom: 0;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+td.tract-image,
+td.tract-circle1,
+td.tract-circle2{
+	text-align: center;
+}
+td.rtl{
+	text-align: right;
+
+}
+
+a{
+	color:#aa0040;
+}
+
+img.tract-image{
+	width: 70%;
+	max-width: 300px;
+}
+
+.tract-image-image{
+	max-width: 90%;
+  height: auto;
+  width: auto\9; /* ie8 */
+}
+.tract-image-circle-image{
+ max-width: 90%;
+  height: auto;
+  width: auto\9; /* ie8 */
+}
+
+
+
+.tract-lawheading{
+	font-size: 1.1em;
+	font-weight: bold;
+
+	color: #0d9ecc;
+
+}
+.tract-law,
+.mylanguage
+{
+	font-size: 1.1em;
+	line-height:1.3em;
+	font-weight: bold;
+	color: #005C86;
+
+
+}
+.tract-law-number{
+	font-size: 3em;
+	color: #f65058;
+	width:20px;
+
+}
+
+.mylanguage{
+	padding-left: 15px;
+	padding-right: 15px;
+}
+.tract-countryname,
+.tract-heading{
+	font-size: 1.1em;
+	line-height:1.3em;
+	color: #f65058;
+	font-weight: bold;
+}
+.tract-verse{
+	font-size: 1em;
+	line-height:1.3em;
+	font-style: italic;
+	color: green;
+	color: black
+
+}
+.tract-text{
+	font-size: 1em;
+	color: #000;
+
+}
+.tract-explanation{
+	font-size: .8em;
+	color: #000;
+
+}
+.tract-circle-image{
+		text-align:center;
+}
+p {
+	margin: 10px;
+
+}
+p.tract-circle-heading-ltr,
+p.tract-circle-heading-rtl,
+.tract-circle-heading
+{
+	font-size: 1.1em;
+    color: #f8aa00;
+    color: #f65058;
+	line-height:1.3em;
+	text-align:center;
+	font-weight: bold;
+}
+p.tract-circle-subhead,
+.tract-circle-image
+{
+	font-size: 1em;
+	color: #000;
+	text-align:center;
+	font-weight: bold;
+}
+p.tract-circle-point-ltr,
+.tract-circle-text
+{
+	font-size: 1em;
+	color: #000;
+	text-align:left;
+}
+p.tract-circle-point-rtl
+{
+	font-size: 1em;
+	color: #000;
+	text-align:right;
+	direction: rtl;
+}
+p.rtl{
+	text-align: right;
+
+}
+.tract-circle-footer-ltr{
+	font-size: 1em;
+	color: #000;
+	text-align:center;
+	font-weight: normal;
+}
+.tract-circle-footer-rtl{
+	font-size: 1em;
+	color: #000;
+	text-align:center;
+	font-weight: normal;
+	direction: rtl;
+}
+.tract-circle-explanation,
+.tract-circle-explanation-ltr,
+.tract-circle-explanation-rtl{
+	font-size: .8em;
+	color: #000;
+	text-align:center;
+}
+.tract-heading.translation,
+.tract-verse.translation,
+.translation{
+	color: #aaa;
+
+}
+.tract-prayer,
+.tract-address,
+.bold{
+	font-weight: bold;
+}
+
+a.tract-link{
+	color: #005C86;
+}
+
+p.tract-circle-point-ltr,
+p.tract-circle-point-rtl
+ {
+  text-indent: -1.3em;
+  padding-left: 1.3em;
+}
+.background{
+	margin-left:15px;
+	margin-right:15px;
+}
+li.background{
+	margin-left:15px;
+}
+li.rtl{
+		direction:rtl;
+
+	}
+h3{
+  margin-top: 1em;
+}
+p.background{
+	font-size: 1em;
+}
+div.content{
+	padding-left:7px;
+	padding-right:7px;
+}
+
+
+@media screen and (min-width: 0em) and (max-width: 30.236em) {
+	body {
+		font-size: .85em;
+		line-height: 1em;
+	}
+	link,
+	.tract-law{
+		font-size: 1em;
+	}
+	li,
+	.tract-prayer,
+	p.tract-circle-point-ltr,
+	p.tract-circle-point-rtl,
+	.tract-circle-text,
+	.tract-circle-footer-ltr,
+	.tract-circle-footer-rtl,
+	p,
+	p.tract-verse,
+	.tract-verse,
+	.tract-text,
+	.tract-countryname,
+	.tract-heading {
+		font-size: .85em;
+		line-height: 1.1em;
+	}
+
+	.tract-law-number {
+		font-size: 2em;
+        width:7px;
+		padding-top:5px;
+	}
+	.title,
+	.tract-list-tpl{
+		font-size:1.4em;
+		line-height:1.6em;
+	}
+	td.side-right, th.side-right {
+		border-bottom: 0;
+		width: 50%;
+		padding-left: 10px;
+		padding-right: 3px;
+	}
+	td.side-left, th.side-left {
+		border-bottom: 0;
+		padding-left: 3px;
+		width: 50%;
+		padding-right: 10px;
+	}
+	img.tract-circle-image{
+		width:50%;
+		max-width:100px;
+	}
+	li {
+		margin: 3px 3px;
+	}
+	li.background {
+		font-size: 1em;
+		line-height: 1.3em;
+	}
+	ul.tract-ul{
+		padding-left:10px;
+	}
+
+	ul, menu, dir {
+		-webkit-padding-start: 10px;
+	}
+	div.content{
+		padding-left:5px;
+		padding-right:5px;
+	}
+}
+</style>
